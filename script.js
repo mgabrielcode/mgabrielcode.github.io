be43ee3ba7aeda76003a1429766d41789a4e1062
marcosgabriel318 <marcosgabriel.mg318@gmail.com>
2024-03-22 19:02:03 -0300

    Refatorando código e corrigindo bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector(".menu-icon");
 const menuNav = document.querySelector("nav");
+const menu = document.querySelectorAll(".tabs li");
 
-const li_switcher = document.querySelectorAll("[data-switcher]")
 
 menuBtn.addEventListener('click', ()=>{
     if(menuNav.style.display == "none"){
@@ -11,4 +11,16 @@ menuBtn.addEventListener('click', ()=>{
     }
 })
 
+
+(function(){
+    for(let i = 0; i < menu.length; i++){
+        const li = menu[i];
+        li.addEventListener('click', () => {
+            document.querySelector('li.is-active').classList.remove('is-active');
+            li.classList.add('is-active');
+            menuNav.style.display = "none";
+        })
+    }
+s 
+})();
 
